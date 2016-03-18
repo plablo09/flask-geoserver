8b9b2e7b48e7a6f1871fb1f023be496eab2c1aec
plablo09 <pablo.lopez@centrogeo.edu.mx>
2016-03-18 11:40:35 -0600

    Entorno virtual completo.

--- Vagrantfile
+++ Vagrantfile
@@ -30,6 +30,8 @@ Vagrant.configure("2") do |config|
 
 	config.vm.provision "shell", path: "provision/bootstrap.sh"
 	config.vm.network "forwarded_port", guest: 5000, host: 5000
+	config.vm.network "forwarded_port", guest: 80, host: 8181
+	config.vm.network "forwarded_port", guest: 8080, host: 9090
 	#config.vm.network "forwarded_port", guest: 8000, host: 8000
 	#config.vm.network "forwarded_port", guest: 8181, host: 8181
 	#config.vm.network :forwarded_port, host: 7111, guest: 5432
--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -1,12 +1,32 @@
+#!/bin/bash
 sudo apt-get update
 #install and configure apache, tomcat and geoserver
-sudo apt-get -y install apache2 tomcat7 tomcat7-admin
-wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.4.0/geoserver-2.4.0-war.zip
-unzip geoserver-2.4.0-war.zip geoserver.war
-sudo mv geoserver.war /var/lib/tomcat7/webapps/
-sudo a2enmod proxy proxy_ajp
-sudo echo ProxyPass '/geoserver ajp://localhost:8009/geoserver' > /etc/apache2/conf.d/proxy.conf
+if ! type "apache2" > /dev/null; then
+    echo "Apache already installed"
+else
+    sudo apt-get -y install apache2
+fi
+
+if ! [ -f "/etc/tomcat7/server.xml" ]; then
+  sudo apt-get -y install tomcat7 tomcat7-admin
+fi
+
+
+sudo apt-get -y install unzip
+if [ -f "/home/vagrant/geoserver-2.8.2-war.zip" ]
+then
+	echo "geoserver alredy found."
+else
+	wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.8.2/geoserver-2.8.2-war.zip
+fi
+
+unzip /home/vagrant/geoserver-2.8.2-war.zip geoserver.war
+sudo mv /home/vagrant/geoserver.war /var/lib/tomcat7/webapps/
+sudo a2enmod proxy proxy_http
+sudo rm /etc/apache2/sites-available/000-default.conf
+sudo cp /vagrant/provision/000-default.conf /etc/apache2/sites-available/000-default.conf
 sudo service apache2 restart
+sudo service tomcat7 restart
 
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
